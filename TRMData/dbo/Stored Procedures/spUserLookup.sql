af8f56208a5e7af857ae2bc39fff9178f2578ef9
Ioan Scafa <ioan.scafa@soter.ro>
2021-10-04 12:25:49 +0300

    Add api UserController and DataAcess
    DataAccess layer added in TRMDataManager.Library

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128) = 0
+AS
+begin
+	set nocount on;
+
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	from [dbo].[User]
+	where Id = @Id
+	
+end
+RETURN 0
