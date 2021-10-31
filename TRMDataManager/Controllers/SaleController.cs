4898984c7775c19022cf25dacf3382e2ce22f9a9
Ioan Scafa <ioan.scafa@soter.ro>
2021-10-31 16:16:22 +0200

    Add User Roles
    Add authorization by role to endpoints.

--- TRMDataManager/Controllers/SaleController.cs
+++ TRMDataManager/Controllers/SaleController.cs
@@ -13,6 +13,7 @@ namespace TRMDataManager.Controllers
     [Authorize]
     public class SaleController : ApiController
     {
+        [Authorize(Roles = "Cashier")]
         public void Post(SaleModel sale)
         {
             SaleData data = new SaleData();
@@ -21,9 +22,11 @@ namespace TRMDataManager.Controllers
             data.SaveSale(sale, userId);
         }
 
+        [Authorize(Roles = "Admin,Manager")]
         [Route("GetSalesReport")]
         public List<SaleReportModel> GetSaleReport()
         {
+            RequestContext.Principal.IsInRole("Admin");
             SaleData data = new SaleData();
             return data.GetSaleReport();
         }
